7e02b425a5c370be69eede17cbc5cbf16afe0d19
Vrutansh Patel <vrutanshpatel01@gmail.com>
2023-11-22 16:50:09 +0530

    loader done

--- script.js
+++ script.js
@@ -1,21 +1,61 @@
+function Revealtospan(){
+    document.querySelectorAll(".reveal")
+.forEach(function(elem){
+    //create two spans
+     var p = document.createElement("span");
+     var c = document.createElement("span");
+
+     //parent and child set their respective classes
+
+     p.classList.add("parent")
+     c.classList.add("child")
+
+     //span parent gets child and child gets elem info
+     c.innerHTML = elem.innerHTML;
+     p.appendChild(c)
+
+     // elem replaces its value with p span
+     elem.innerHTML = "";
+     elem.appendChild(p)
+});
+}
+
+Revealtospan()
+
 var tl = gsap.timeline();
 
-tl.to("#fs",{
-    height:0,
-    duration:2,
-    ease:Expo.easeInOut
+tl
+.from(".child span",{
+    x: 100,
+    stagger:.2,
+    duration: 1.4,
+    ease: Power3.easeInOut
 })
 
-tl.to("#elem",{
-    height:"100%",
-    duration:2,
-    delay:-2,
-    ease:Expo.easeInOut
+.to(".parent .child",{
+    y: "-100%",
+    duration: 1,
+    ease: Circ.easeInOut
 })
 
-tl.to("#white",{
-    height:"100%",
-    duration:2,
-    delay:-1.6,
-    ease:Expo.easeInOut
+.to("#loader",{
+    height: 0,
+    duration: 1,
+    ease: Circ.easeInOut
+})
+
+.to("#green",{
+    height: "100%",
+    top: 0,
+    duration: .89,
+    delay: -.9,
+    ease: Circ.easeInOut
+})
+
+.to("#green",{
+    height: "0%",
+    top: 0,
+    duration: .89,
+    delay:-.4,
+    ease: Circ.easeInOut
 })
